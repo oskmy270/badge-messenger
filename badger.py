53080caec5e7f4e648ea0a013c4ec4d8af6bf0b5
Oskar Myrberg <oskar@myrberg.info>
2014-11-27 10:14:40 +0100

    Initial test of badger.py

--- badger.py
+++ badger.py
@@ -2,51 +2,53 @@ import os
 import time
 import badge
 DEVICE = "/dev/ttyUSB0"
+rssFeed = 'http://www.dn.se/nyheter/m/rss'
 def main():
 	currentMessage = 'Starting...'
-	writeToLed(currentMessage)
+	writeToLed(currentMessage,'2')
+	time.sleep(2)
 	while True:
 		dropboxInfo = checkDropBox()
-		if dropboxInfo == 'news'
-			wantedMessage = fetchRss()
+		if dropboxInfo[0:3] == 'news'
+			wantedMessage = fetchRss(dropBoxInfo.split()[1])
 		else:
 			wantedMessage = dropboxInfo
 		if wantedMessage != currentMessage:
-			writeToLed(wantedMessage)
+			writeToLed(wantedMessage,'4')
 			currentMessage = wantedMessage
-def writeToLed(msg):
+		print 'Now sleeping'
+		for i in range(0,10):
+			print '.'
+			time.sleep(1)
+	print '.'
+def writeToLed(msg,spd):
 	os.system("stty speed 38400 <" + DEVICE)
-while True:
+	f = open(DEVICE, "w")
+	pkts = badge.build_packets(0x600, badge.message_file(msg, speed=spd, action=badge.ACTION_ROTATE))
+	for p in pkts:
+		f.write(p.format())
+	f.flush()
+	f.close()
+
+def checkDropBox():
 	print 'Now connecting to dropbox'
 	os.system("wget -q https://dl.dropboxusercontent.com/u/2082167/ledbadge.txt -N")
 	file = open("ledbadge.txt","r")
 	dropboxInfo = file.read()
 	file.close()
-	if dropboxInfo == 'news':
-		badgeMessage = 'Nyheter: '
-		os.system('python rssFetcher.py')
-		newsFile = open('news.txt','r')
-		badgeMessage = badgeMessage+newsFile.read()
-		newsFile.close()
-	else:
-		badgeMessage = dropboxInfo
-
-	print 'Comparing messages'
-	print 'Current:', currentText
-	print 'Wanted: ', badgeMessage
-
-	if currentText != badgeMessage:
-		print 'Now trying to write to badge, ', badgeMessage
-		#os.system("stty speed 38400 <" + DEVICE)
-		f = open(DEVICE, "w")
-		pkts = badge.build_packets(0x600, badge.message_file(badgeMessage, speed='4', action=badge.ACTION_ROTATE))
-		for p in pkts:
-			f.write(p.format())
-		f.flush()
+	
+def fetchRss(feed):
+	if feed == 'http://www.dn.se/nyheter/m/rss':
+	os.system('wget '+feed+' -O rss')
+		f = open('rss','r')
+		for line in f:
+			temp = line.find('[CDATA')
+			if temp != -1 and line.find('<img') == -1:
+				message =  line[temp:].split('[')[2].split(']')[0]
+				if message.find('Nyheter - Nyheter') == -1:
+					return message
 		f.close()
-		currentText = badgeMessage
-	print 'Now sleeping'
-	for i in range(0,10):
-		print '.',
-		time.sleep(1)
-	print '.'
+	else:
+		return 'Feed not supported'
+	
+	
