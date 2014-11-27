c352c67cfb51f54d464d107713f522a699d99ffb
Oskar Myrberg <oskar.myrberg@gmail.com>
2014-11-27 11:04:49 +0100

    Fixed some bugs

--- badger.py
+++ badger.py
@@ -12,7 +12,7 @@ def main():
 		if dropboxInfo[0:4].find('news') != -1:
 			print 'News detected'
 			print dropboxInfo
-			wantedMessage = 'Nyheter: '+fetchRss(dropboxInfo.split()[1])
+			wantedMessage = fetchRss(dropboxInfo.split()[1])
 		else:
 			wantedMessage = dropboxInfo
 		if wantedMessage == currentMessage:
@@ -52,8 +52,18 @@ def fetchRss(feed):
 			if temp != -1 and line.find('<img') == -1:
 				message =  line[temp:].split('[')[2].split(']')[0]
 				if message.find('Nyheter - Nyheter') == -1:
-					return message
+					return 'DN: '+message
 		f.close()
+	
+	elif feed == 'http://rss.cnn.com/rss/edition.rss':
+		os.system('wget '+feed+' -O rss')
+		f = open('rss','r')
+		for line in f:
+			temp = line.find('<title>')
+			if temp != -1:
+				message = line[temp+6:]
+				if message.find('CNN.com - Top Stories') == -1:
+					return 'CNN: '+message
 	else:
 		return 'Feed not supported'
 	
